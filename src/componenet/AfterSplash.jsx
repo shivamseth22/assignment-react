2338fbe9ad8f54a691b4cde84154bfb72bbae3f8
Shivam seth <shivamseth221104@gmail.com>
2023-12-10 08:35:00 +0530

    updated

---
+++ src/componenet/AfterSplash.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AfterSplash = () => {
+  return (
+    <div className='bg-blue-500 m-0 p-0 '>
+        cx 
+    </div>
+  )
+}
+
+export default AfterSplash
--- src/componenet/Body.jsx
+++ src/componenet/Body.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Login from './Login'
 import LoginPage from './LoginPage'
 import Sucessfull from './Sucessfull'
+import AfterSplash from './AfterSplash'
 
 const Body = () => {
   return (
     // <div><LoginPage/></div>
-    <div><Sucessfull/></div>
+    // <div><Sucessfull/></div>
+    <div><AfterSplash/></div>
   )
 }
 
