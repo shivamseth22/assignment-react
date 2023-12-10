c859f48a0042d69fbe4821cf55600b6d2457470c
Shivam seth <shivamseth221104@gmail.com>
2023-12-10 23:54:21 +0530

    ok

--- src/component/City.jsx
+++ src/component/City.jsx
@@ -4,14 +4,18 @@ const City = ({cityData , SetCityData}) => {
   const myRef = useRef(null);
   useEffect(()=>{
     const eventHandler = (e) =>{
-      if(!myRef.current.contains(e.target)){
-        SetCityData(false);
+      if (
+        cityData &&
+        myRef.current &&
+        !myRef.current.contains(e.target)
+      ) {
+        SetCityData(false)
       }
     }
-    document.addEventListener("mousedown" , eventHandler )
+    document.addEventListener('mousedown', eventHandler)
   },[])
   return (
-    <div  ref={myRef} className="p-[3rem] flex flex-col gap-5 absolute bg-white mt-12 text-black myCityData">
+    <div  ref={myRef} className="p-[3rem] flex flex-col gap-5 absolute bg-white mt-12 text-black myCityData z-10">
       <div className="flex bg-red-300 items-center justify-center ">
         <p >X</p>
         <input type="text" className="bg-gray-400" />
