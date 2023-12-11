4113e219b492d5b1834ce72091f6e97c53110c16
Shivam seth <shivamseth221104@gmail.com>
2023-12-11 09:56:24 +0530

    ok

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-
+import Icon from "./Icon";
 const Detail = () => {
   const [myId, setMyId] = useState(null);
   const [localLiveData, setLocalLiveData] = useState(null);
+  const [apiData, setApiData] = useState();
+  console.log(apiData);
 
   const weatherDetails = async () => {
     if (myId) {
-      const token =JSON.parse(localStorage.getItem("myToken"));
+      const token = JSON.parse(localStorage.getItem("myToken"));
       const response = await fetch(
         `https://hiring-test.a2dweb.com/view-small-forecast/${myId}`,
         {
@@ -19,7 +21,8 @@ const Detail = () => {
         }
       );
       const data = await response.json();
-      console.log(data);
+      console.log(data.DATA[0].data1.time);
+      setApiData(data.DATA);
     }
   };
 
@@ -40,8 +43,45 @@ const Detail = () => {
       <div className="flex justify-around">
         <p>Today</p>
         <p>sep 12</p>
+        {/* <p>{apiData.Data[0].}</p> */}
       </div>
-      <div>data</div>
+      {apiData?.map((data) => {
+        return (
+          <div key={Math.random()} className="flex">
+            <div>
+              <h1>{data?.data1?.time}</h1>
+              <Icon condition={"sunny"} />
+              <p>{data?.data1?.temperature}</p>
+            </div>
+            <div >
+              <h1>{data?.data2?.time}</h1>
+              <Icon condition={"sunny"} />
+              <p>{data?.data2?.temperature}</p>
+            </div>
+            <div>
+              <h1>{data?.data3?.time}</h1>
+              <Icon condition={"sunny"} />
+              <p>{data?.data3?.temperature}</p>
+            </div>
+            <div>
+              <h1>{data?.data4?.time}</h1>
+              <Icon condition={"sunny"} />
+              <p>{data?.data4?.temperature}</p>
+            </div>
+            <div>
+              <h1>{data?.data5?.time}</h1>
+              <Icon condition={"sunny"} />
+              <p>{data?.data5?.temperature}</p>
+            </div>
+            <div>
+              <h1>{data?.data1?.time}</h1>
+              <Icon condition={"sunny"} />
+              <p>{data?.data1?.temperature}</p>
+            </div>
+          </div>
+        );
+      })}
+
       <div className="flex justify-around">
         <p>nextForecast</p>
         <p>dd</p>
